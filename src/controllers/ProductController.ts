2e3f44739108c36e29923a92e6662729738ad307
Lucas Benkendorf <benklucass@gmail.com>
2025-03-03 20:13:08 -0300

    adicionado rota para listagem de produtos

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -53,3 +53,41 @@ export const createProduct = async (
     res.status(500).json({ error: "Erro interno no servidor." });
   }
 };
+
+export const listProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req as any;
+
+  try {
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOne({
+      where: { id: userId },
+      relations: ["branch"],
+    });
+
+    if (!user) {
+      res.status(404).json({ error: "Usuário não encontrado." });
+      return;
+    }
+
+    if (!user.branch) {
+      res
+        .status(403)
+        .json({ error: "Usuário não está vinculado a uma filial." });
+      return;
+    }
+
+    const productRepository = AppDataSource.getRepository(Product);
+    const products = await productRepository.find({
+      where: { branch: { id: user.branch.id } },
+      relations: ["branch"],
+    });
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Erro durante a listagem dos produtos:", error);
+    res.status(500).json({ error: "Erro interno no servidor." });
+  }
+};
